bc1ecf2f502662b634954409e6f5772f248e92ee
tty-github <imgnt.vs@gmail.com>
2015-06-29 15:17:31 +0900

    init

---
+++ pac/proxy.js
@@ -0,0 +1,26 @@
+var black_domains = [
+	"astrsk.net"
+,	"socdm.com"
+,	"adstore.jp"
+,	"astrsk.net"
+,	"iadsdk.apple.com"
+,	"iadc.qwapi.com"
+,	"amebaspgame.com"
+,	"c1.nend.net"
+,	"output.nend.net"
+,	".i-mobile.co.jp"
+,	"googleads.g.doubleclick.net"
+];
+
+function FindProxyForURL(url, host) {
+	var direct	= "DIRECT";
+	var blackhole	= "PROXY 8.8.8.8:53;";
+
+	for (var i = 0, len = black_domains.length; i < len; i++ ) {
+		if(dnsDomainIs(host, black_domains[i])) {
+			return blackhole;
+		}
+	}
+
+	return direct;
+}
